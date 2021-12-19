66112865bd385ea192548fcb36f0f2f61b34b4b1
Yona-Dav <yona.bohbot@gmail.com>
2021-12-19 21:47:55 +0200

    change front end

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('homepage/', views.ContactCreateView.as_view(), name='homepage'),
+    path('', views.ContactCreateView.as_view(), name='homepage'),
     path('contact/', views.contact_thanks, name='contact')
 
 ]
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -142,7 +142,7 @@ except ImportError:
     EMAIL_USE_TLS = True
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD =os.environ.get('EMAIL_HOST_PASSWORD')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
     EMAIL_PORT = 587
 
     RECAPTCHA_PUBLIC_KEY = os.environ.get(str('RECAPTCHA_PUBLIC_KEY'))
